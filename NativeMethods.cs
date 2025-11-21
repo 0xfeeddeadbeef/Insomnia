33ed5d248fd0483566df9c08c872556a6e1b3b9a
Giorgi Chakhidze <0xfeeddeadbeef@gmail.com>
2025-11-21 14:18:13 +0400

    Add project files.

---
+++ NativeMethods.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Insomnia;
+
+internal static partial class NativeMethods
+{
+    [LibraryImport("kernel32.dll", SetLastError = false)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvStdcall)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+    internal static partial EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+}
+
+internal enum EXECUTION_STATE : uint
+{
+    NONE = 0x00000000u,
+    ES_SYSTEM_REQUIRED = 0x00000001u,
+    ES_DISPLAY_REQUIRED = 0x00000002u,
+    ES_USER_PRESENT = 0x00000004u,
+    ES_AWAYMODE_REQUIRED = 0x00000040u,
+    ES_CONTINUOUS = 0x80000000u,
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static Insomnia.NativeMethods;
+
+namespace Insomnia;
+
+internal static class Program
+{
+    private static int Main()
+    {
+        var prevState = SetThreadExecutionState(
+            EXECUTION_STATE.ES_CONTINUOUS |
+            EXECUTION_STATE.ES_SYSTEM_REQUIRED |
+            EXECUTION_STATE.ES_AWAYMODE_REQUIRED |
+            EXECUTION_STATE.ES_DISPLAY_REQUIRED);
+
+        if (prevState == EXECUTION_STATE.NONE)
+        {
+            Console.Error.WriteLine("Error: Failed to set the thread execution state.");
+            return 1;
+        }
+
+        Console.WriteLine("Press [Enter] to allow sleeping");
+        _ = Console.ReadLine();
+
+        prevState = SetThreadExecutionState(prevState);
+        if (prevState == EXECUTION_STATE.NONE)
+        {
+            Console.Error.WriteLine("Error: Failed to reset the thread execution state.");
+            return 1;
+        }
+
+        return 0;
+    }
+}
